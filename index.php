4e5fb5826f0bd82f2110ccdd177803b3accc905e
odczik <ondrej.dostal007@gmail.com>
2025-03-17 15:40:04 +0100

    Basic router

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+require "./src/functions/router.php";
+
+$router = new Router();
+
+$router->get("/", function() {
+    return render("./src/views/landing.php");
+});
+
+$router->dispatch();
+
+// Helper function to render pages
+function render($path, $data = []) {
+    ob_start();
+    require $path;
+    return ob_get_clean();
+}
---
+++ src/functions/router.php
@@ -0,0 +1,24 @@
+<?php
+class Router {
+    private $routes = [];
+
+    public function get($path, $callback) {
+        $this->routes["GET"][$path] = $callback;
+    }
+
+    public function post($path, $callback) {
+        $this->routes["POST"][$path] = $callback;
+    }
+
+    public function dispatch() {
+        $method = $_SERVER["REQUEST_METHOD"];
+        $path = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
+
+        if (isset($this->routes[$method][$path])) {
+            echo call_user_func($this->routes[$method][$path]);
+        } else {
+            http_response_code(404);
+            echo "404 Not Found";
+        }
+    }
+}
