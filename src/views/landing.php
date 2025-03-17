cf135f38ed28c69480a53fc469acb8bac863c1fb
odczik <ondrej.dostal007@gmail.com>
2025-03-17 15:44:43 +0100

    Basic components

--- src/views/landing.php
+++ src/views/landing.php
@@ -6,7 +6,10 @@
     <title>Landing page</title>
 </head>
 <body>
-    <?php include 'src/views/components/navbar.php'; ?>
+    <?php include 'src/views/components/navbar.phtml'; ?>
+
     <h1>Welcome</H1>
+
+    <?php include 'src/views/components/footer.phtml'; ?>
 </body>
-</html>
+</html> 
