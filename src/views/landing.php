d6ad53a7c04380bfbab88c44ed91cc7dc85218b5
odczik <58082541+odczik@users.noreply.github.com>
2025-03-18 13:42:04 +0100

    Update landing.php

--- src/views/landing.php
+++ src/views/landing.php
@@ -4,6 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Landing page</title>
+    <link rel="stylesheet" href="public/css/style.css">
+    <link rel="stylesheet" href="public/css/navbar.css">
+    <link rel="stylesheet" href="public/css/footer.css">
 </head>
 <body>
     <?php include 'src/views/components/navbar.phtml'; ?>
