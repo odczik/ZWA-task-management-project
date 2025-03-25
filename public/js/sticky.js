d11d662a73039ffaa611e18497216b6433092252
odczik <58082541+odczik@users.noreply.github.com>
2025-03-25 14:16:06 +0100

    Navbar account menu

--- public/js/sticky.js
+++ public/js/sticky.js
@@ -1,6 +1,6 @@
 const stickyElement = document.querySelector('nav');
 const observer = new IntersectionObserver(
   ([e]) => e.target.classList.toggle('is-sticky', e.intersectionRatio < 1),
-  { threshold: [1] }
+  { rootMargin: '0px 0px 0px 0px', threshold: [1] }
 );
 observer.observe(stickyElement);
