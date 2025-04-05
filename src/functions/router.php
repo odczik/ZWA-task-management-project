3f84e2fd5a72e6273beeb68235e0c32397dd3a1e
odczik <ondrej.dostal007@gmail.com>
2025-04-05 13:05:02 +0200

    Router tiddy up

--- src/functions/router.php
+++ src/functions/router.php
@@ -2,31 +2,17 @@
 class Router {
     private $routes = [];
 
-    public function get($path, $callback) {
-        $this->routes["GET"][$path] = $callback;
-    }
-
-    public function post($path, $callback) {
-        $this->routes["POST"][$path] = $callback;
-    }
-
-    public function put($path, $callback) {
-        $this->routes["PUT"][$path] = $callback;
-    }
-
-    public function delete($path, $callback) {
-        $this->routes["DELETE"][$path] = $callback;
-    }
-
-    public function patch($path, $callback) {
-        $this->routes["PATCH"][$path] = $callback;
-    }
+    public function get    ($path, $callback) { $this->routes["GET"]    [$path] = $callback; }
+    public function post   ($path, $callback) { $this->routes["POST"]   [$path] = $callback; }
+    public function put    ($path, $callback) { $this->routes["PUT"]    [$path] = $callback; }
+    public function delete ($path, $callback) { $this->routes["DELETE"] [$path] = $callback; }
+    public function patch  ($path, $callback) { $this->routes["PATCH"]  [$path] = $callback; }
 
     public function dispatch() {
         $method = $_SERVER["REQUEST_METHOD"];
         $path = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
 
-        // Parse the request body for non-GET methods
+        // Parse the request body
         $body = file_get_contents("php://input");
         $data = $body ? json_decode($body, true) : [];
 
