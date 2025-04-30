1cd021da53600fc9f44e4b19a189ef75c0c93a7c
odczik <58082541+odczik@users.noreply.github.com>
2025-04-30 21:45:01 +0200

    Landing page overhaul

--- src/views/landing.php
+++ src/views/landing.php
@@ -13,12 +13,48 @@
     <?php include 'src/views/components/navbar.phtml'; ?>
 
     <header>
-        <h1>Welcome</h1>
-        <p>This is the landing page</p>
+        <h1>Mango</h1>
+        <p>Interaktivní <b>správce projektů</b> pro týmy i individuály.</p>
+        <button>Začínáme</button>
     </header>
 
     <main class="home-content">
-        Some content
+        <div class="landing-text">
+            <div>
+                <p>Do předmětu <i>Základy webových aplikací</i>.</p>
+                <p>Projekt je zaměřen na správu projektů a úkolů. Umožňuje uživatelům vytvářet projekty, přidávat úkoly a spravovat členy projektů.</p>
+            </div>
+            <img src="public/landing-task.png" alt="Sample task" class="task-image">
+        </div>
+        <p>
+            Vytvořeno pomocí <b>nejnovějších&trade;</b> technologií jako jsou např. PHP.. počkat.. beru zpátky.
+            <br>
+            (ale vážně, je zázrak že PHP přežilo Y2K jaktože je pořád relevantní)
+        </p>
+        <p>60% času to funguje na 100% 👍</p>
+        <div class="landing-text">
+            <img src="public/howard-technology.png" alt="Howard Stark" class="technology-image">
+            <div>
+                <div style="display: flex; flex-direction: row;">
+                    <div>
+                        <p>Backend</p>
+                        <ul>
+                            <li>PHP</li>
+                            <li>MySQL</li>
+                        </ul>
+                    </div>
+                    <div>
+                        <p>Frontend</p>
+                        <ul>
+                            <li>HTML</li>
+                            <li>CSS</li>
+                            <li>JavaScript</li>
+                        </ul>
+                    </div>
+                </div>
+                <p>Takový návrat k opicím 🙈</p>
+            </div>
+        </div>
     </main>
 
     <?php include 'src/views/components/footer.phtml'; ?>
