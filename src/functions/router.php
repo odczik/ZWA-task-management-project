8e08e2986f833e4de67275ad7ae20a993e4ddc2b
odczik <58082541+odczik@users.noreply.github.com>
2025-05-20 12:28:22 +0200

    Project presentation preparation

--- src/functions/router.php
+++ src/functions/router.php
@@ -1,13 +1,16 @@
 <?php
 class Router {
+    // List of routes
     private $routes = [];
 
+    // Define the HTTP methods
     public function get    ($path, $callback) { $this->routes["GET"]    [$path] = $callback; }
     public function post   ($path, $callback) { $this->routes["POST"]   [$path] = $callback; }
     public function put    ($path, $callback) { $this->routes["PUT"]    [$path] = $callback; }
     public function patch  ($path, $callback) { $this->routes["PATCH"]  [$path] = $callback; }
     public function delete ($path, $callback) { $this->routes["DELETE"] [$path] = $callback; }
 
+    // Dispatch the request to the appropriate route
     public function dispatch() {
         $method = $_SERVER["REQUEST_METHOD"];
         $path = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
