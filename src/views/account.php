944e3cf094ebf30fe84d84ce5d3b0d0f83e3e82f
odczik <58082541+odczik@users.noreply.github.com>
2025-06-10 12:55:57 +0200

    Update account.php

--- src/views/account.php
+++ src/views/account.php
@@ -46,7 +46,7 @@ if(!$jwtHandler->isLoggedIn()) {
                     <input type="file" name="profile" id="profile" accept="image/*" onchange="document.getElementById('profile-preview').src = window.URL.createObjectURL(this.files[0])">
                 </div>
             </div>
-            <button onclick="confirm('Are you sure? This action cannot be undone.') ? fetch('/api/account', {method: 'DELETE'}) : null" style="position: absolute; left: 15px; bottom: 15px; color: var(--error-dark); border: none; background: none;">Delete account</button>
+            <button onclick="confirm('Are you sure? This action cannot be undone.') ? fetch('/api/account', {method: 'DELETE'}) : null" style="position: absolute; left: 20px; bottom: 20px; color: var(--error-dark); border: none; background: none; cursor: pointer; font-weight: 500;">Delete account</button>
             <span class="buttons">
                 <button type="button" onclick="location.reload()">Cancel</button>
                 <button type="submit">Save Changes</button>
